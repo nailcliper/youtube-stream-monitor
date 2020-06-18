be0df9ed97f275a3c8c280542e261a4c67d1320f
nailcliper <ltayanami@hotmail.com>
2020-06-17 20:00:08 -0700

    Add condition for channels that don't have any videos

--- xml_parser.py
+++ xml_parser.py
@@ -8,18 +8,18 @@ def xml_parse(id):
     try:
         response = http.request('GET',"https://www.youtube.com/feeds/videos.xml?channel_id="+id)
         data = xmltodict.parse(response.data)
-        
         feed = data['feed']
         title = feed['title']   #channel name
-        entry = feed['entry']   #videos
-        
-        output = {}
-        output['channel'] = title
-        output['videos'] = set()
+        if "entry" in feed:
+            entry = feed['entry']   #videos
+            
+            output = {}
+            output['channel'] = title
+            output['videos'] = set()
 
-        for video in entry:
-            v = video['yt:videoId']
-            output['videos'].add(v)
+            for video in entry:
+                v = video['yt:videoId']
+                output['videos'].add(v)
     except:
         print("Failed to parse xml from response, id: "+id)
     
